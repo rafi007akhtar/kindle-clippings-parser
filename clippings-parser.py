71ce276956889153048d1bb8817aae73147a2a11
Md Rafi Akhtar <alimdrafi@gmail.com>
2022-06-04 22:01:19 +0530

    cleaned the logic a little and added comments

--- clippings-parser.py
+++ clippings-parser.py
@@ -1,16 +1,15 @@
-import re
-from tkinter.messagebox import NO
-from typing import Set
-
+# Location of the clippings file
 CLIPPINGS = 'My Clippings.txt'
-PARSED = 'parsed-results.md'
 
+# constants
 SKIPPABLE_PHRASES = [
     '- Your Highlight at location ',
     '- Your Bookmark at location '
 ]
 NOTE = '- Your Note at location '
+END_OF_HIGHLIGHT = '=========='
 
+# helper methods
 def indexOf(obj, elem):
     try:
         ind = obj.index(elem)
@@ -24,34 +23,39 @@ def elemInLine(listOfElems: list, line: str):
             return True
     return False
 
+# this result will contain all the parsed result
 results = {
     'titles': set()
+    # 'title': [] <- this array will contain all highlights, and will be accessible through the book title
 }
 
-begin = True
+# main logic for putting in the highlights in an array based on the titles
+new_highlight = True
 quote_next_thought = False
-titles = []
-end = '=========='
 current_title = ''
 with open(CLIPPINGS, encoding='utf8') as f:
     for line in f:
+        # clean up each line
         line = line.strip()
         line = line.replace('\ufeff', '')
-        if begin:
+
+        # prase the line based on what it contains
+        if new_highlight:
             results['titles'].add(line)
             current_title = line
-            begin = False
-        elif line in ['\n', '', ' ']:
+            new_highlight = False
+        elif (
+            line in ['\n', '', ' '] or
+            elemInLine(SKIPPABLE_PHRASES, line)
+        ):
             continue
-        elif line == end:
-            begin = True
+        elif line == END_OF_HIGHLIGHT:
+            new_highlight = True  # next one will be a new highlight since we reached the end for the current one
             if current_title in results:
                 results[current_title].append('\n')
             continue
-        elif elemInLine(SKIPPABLE_PHRASES, line):
-            continue
         elif indexOf(line, NOTE) == 0:
-            quote_next_thought = True
+            quote_next_thought = True  # clippings file announcing the next line is my note, so it should apply the quotes markdown style
             continue
         else:
             if quote_next_thought:
@@ -62,10 +66,13 @@ with open(CLIPPINGS, encoding='utf8') as f:
             results[current_title].append(line)
 
 
+# prepend the title of the book as a heading to its highlights
 for title in results['titles']:
     title_text = f'# {title}'
     results[title].insert(0, title_text)
 
+# method used for creating a new MD file for a title and
+# dumping all highlights there
 def save_highlights(parsed_text, title: str = None):
     if not title:
         title = parsed_text[0][2:]
@@ -82,5 +89,6 @@ def save_highlights(parsed_text, title: str = None):
     results_file.write(lines)
     results_file.close()
 
+# for each title, save the highlights in a new file
 for title in results['titles']:
     save_highlights(results[title])
