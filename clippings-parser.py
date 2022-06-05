c32f2a1f6c8def36309e1d04090862d722090316
Md Rafi Akhtar <alimdrafi@gmail.com>
2022-06-05 15:56:18 +0530

    Restructured results and included highlights
    - The title key of the result will not point to a list now
    - Instead it will point to a dictionary
    - The dictionary will have _two_ key-value pairs:
    	- highlights: a list containing the highlights of this title
    	  (same as the previous list)
    	- meta-data: a dictionary which for now contains a number
    	  tracking the headers
    - This number is used to put the highlight number as an H3 heading, and
      applied throughout the document

--- clippings-parser.py
+++ clippings-parser.py
@@ -1,4 +1,4 @@
-import re, string
+import string
 
 # Location of the clippings file
 CLIPPINGS = 'My Clippings.txt'
@@ -10,6 +10,7 @@ SKIPPABLE_PHRASES = [
 ]
 NOTE = '- Your Note at location '
 END_OF_HIGHLIGHT = '=========='
+HIGHLIGHT_HEADER = '### Highlight #'
 
 # helper methods
 def indexOf(obj, elem):
@@ -27,25 +28,32 @@ def elemInLine(listOfElems: list, line: str):
 
 def line_cleanup(line: str):
     line = line.strip().replace('\ufeff', '')
-    # line = re.sub('[^A-Za-z0-9]+', '', line)
 
     # The following logic is borrowed from:
     # https://stackoverflow.com/questions/92438/stripping-non-printable-characters-from-a-string-in-python
     line = ''.join(filter(lambda c: c in string.printable, line))
+
     return line
 
-    
+def create_new_title():
+    return {
+        'highlights': [],
+        'meta-data': {
+            'highlight-header-number': 0
+        }
+    }
 
 # this result will contain all the parsed results
 results = {
     'titles': set()
-    # title: [] <- this array will contain all highlights, and will be accessible through the book title
+    # title: {} <- this array will contain all highlights, and will be accessible through the book title as key
 }
 
 # main logic for putting in the highlights in an array based on the titles
 new_highlight = True
 quote_next_thought = False
 current_title = ''
+skip_next_header = False
 with open(CLIPPINGS, encoding='utf') as f:
     for line in f:
         # # clean up each line
@@ -66,7 +74,7 @@ with open(CLIPPINGS, encoding='utf') as f:
         elif line == END_OF_HIGHLIGHT:
             new_highlight = True  # next one will be a new highlight since we reached the end for the current one
             if current_title in results:
-                results[current_title].append('\n')
+                results[current_title]['highlights'].append('\n')
             continue
         elif indexOf(line, NOTE) == 0:
             quote_next_thought = True  # clippings file announcing the next line is my note, so it should apply the quotes markdown style
@@ -76,14 +84,23 @@ with open(CLIPPINGS, encoding='utf') as f:
                 line = '> ' + line
                 quote_next_thought = False
             if current_title not in results.keys():
-                results[current_title] = []
-            results[current_title].append(line)
+                results[current_title] = create_new_title()
+            results[current_title]['highlights'].append(line)
+
+            # add the highlight accordingly
+            if skip_next_header:
+                skip_next_header = False
+                continue
+            highlight_ind = indexOf(results[current_title]['highlights'], line)
+            n = results[current_title]['meta-data']['highlight-header-number'] + 1
+            results[current_title]['highlights'].insert(highlight_ind, f'{HIGHLIGHT_HEADER}{n}')
+            results[current_title]['meta-data']['highlight-header-number'] += 1
 
 
 # prepend the title of the book as a heading to its highlights
 for title in results['titles']:
     title_text = f'# {title}'
-    results[title].insert(0, title_text)
+    results[title]['highlights'].insert(0, title_text)
 
 # method used for creating a new MD file for a title and
 # dumping all highlights there
@@ -105,9 +122,9 @@ def save_highlights(parsed_text, title: str = None):
 
 # for each title, save the highlights in a new file
 for title in results['titles']:
-    save_highlights(results[title])
+    save_highlights(results[title]['highlights'])
 
+# experimental code
 exp = results['One Click Agile (ultimatix.net)']
-for line in exp:
+for line in exp['highlights']:
     print(line)
-    # ™
